b3c223033a19cb209319fa25c804ebf1cfac3ca8
avula manidhar <maniyadavrc@gmail.com>
2026-02-23 14:28:59 +0530

    first commit

---
+++ app/src/main/java/com/example/mindguardaipsychologicalsupportapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.mindguardaipsychologicalsupportapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Button continueButton = findViewById(R.id.button);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
